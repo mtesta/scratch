1edc136534536d187c4b3d958c782fde2786fedd
Mark Testa <mtesta89@gmail.com>
2009-01-11 20:59:52 -0500

    Cleaned up file a bit. No real difference

--- test_cpp/hello_world.cpp
+++ test_cpp/hello_world.cpp
@@ -3,8 +3,11 @@
  */
 #include <iostream>
 
-int main()
+using namespace std;
+
+int main(int argc, char **argv)
 {
-    std::cout << "Hello World!" << std::endl;
+    cout << "Hello World!" << endl;
+
     return 0;
 }
